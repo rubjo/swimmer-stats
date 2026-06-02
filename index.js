3f63afafd3d0cd5cadc848951cd57e3b30826d26
rubjo <rune.bjorneras@gmail.com>
2026-06-02 19:32:18 +0200

    Increase Puppeteer protocol timeout to 5 min for slow CI runs

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ async function main() {
   const browser = await puppeteer.launch({
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    protocolTimeout: 120_000,
+    protocolTimeout: 300_000,
   });
   const page = await browser.newPage();
   await page.setUserAgent(
